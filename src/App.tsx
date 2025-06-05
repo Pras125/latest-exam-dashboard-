a1d223862a1c6a5f13df30d888643f308f204f0f
Pras125 <90955869+Pras125@users.noreply.github.com>
2025-06-05 10:17:39 +0530

    Add test authentication flow with login redirection

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import TestLogin from "./pages/test/[id]";
+import TestAttempt from "./pages/test/[id]/attempt";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +18,8 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/test/:id" element={<TestLogin />} />
+          <Route path="/test/:id/attempt" element={<TestAttempt />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
